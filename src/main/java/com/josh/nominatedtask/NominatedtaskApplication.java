e34f5b6f2002be2fb3f4e2c8bdf64c9b617a2f19
Stixxy <100129571+StixxyTape@users.noreply.github.com>
2025-12-01 14:36:08 +0000

    maven setup

---
+++ src/main/java/com/josh/nominatedtask/NominatedtaskApplication.java
@@ -0,0 +1,13 @@
+package com.josh.nominatedtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominatedtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NominatedtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josh/nominatedtask/NominatedtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.nominatedtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominatedtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
